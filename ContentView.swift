a74902b955e9d8530cd8ddfac50ad6b0b5cd5abe
nnl3336 <may.be.be.k12@gmail.com>
2025-11-12 19:12:36 +0900

    commit-o

--- ContentView.swift
+++ ContentView.swift
@@ -9,62 +9,181 @@ import SwiftUI
 import CoreData
 import UIKit
 
+// MARK: - ViewController (UIKit)
 class ViewController: UIViewController, UITextViewDelegate {
     let textView = UITextView()
-    var attachments: [NSTextAttachment] = [] // 画像のリスト
+    var attachments: [NSTextAttachment] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
 
-        textView.frame = view.bounds.insetBy(dx: 20, dy: 60)
+        // UITextView 設定
         textView.isEditable = false
+        textView.isScrollEnabled = true
+        textView.delegate = self
+        textView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(textView)
 
-        let attr = NSMutableAttributedString(string: "Tap images below:\n\n")
+        NSLayoutConstraint.activate([
+            textView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            textView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            textView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            textView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
 
-        // サンプル画像を3枚追加
+        // NSMutableAttributedString に文字と画像を追加
+        let attr = NSMutableAttributedString(string: "Tap images below:\n\n")
         for i in 1...3 {
-            if let image = UIImage(named: "sample\(i)") {
-                let att = NSTextAttachment()
-                att.image = image
-                let ratio = image.size.height / image.size.width
-                att.bounds = CGRect(x: 0, y: 0, width: 150, height: 150 * ratio)
-                attachments.append(att)
-                attr.append(NSAttributedString(attachment: att))
-                attr.append(NSAttributedString(string: "\n\n"))
+            guard let image = UIImage(named: "sample\(i)") else {
+                print("sample\(i) not found")
+                continue
             }
+
+            let attachment = NSTextAttachment()
+            attachment.image = image
+
+            // 幅 150px にリサイズして高さをアスペクト比で計算
+            let maxWidth: CGFloat = 150
+            let ratio = image.size.height / image.size.width
+            attachment.bounds = CGRect(x: 0, y: 0, width: maxWidth, height: maxWidth * ratio)
+
+            attachments.append(attachment)
+            attr.append(NSAttributedString(attachment: attachment))
+            attr.append(NSAttributedString(string: "\n\n"))
         }
+
         textView.attributedText = attr
 
+        // タップジェスチャー
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         textView.addGestureRecognizer(tap)
     }
 
     @objc func handleTap(_ gesture: UITapGestureRecognizer) {
         let location = gesture.location(in: textView)
-        
-        guard let position = textView.closestPosition(to: location) else { return }
+        var loc = location
+        loc.x -= textView.textContainerInset.left
+        loc.y -= textView.textContainerInset.top
 
-        guard let range = textView.tokenizer.rangeEnclosingPosition(
-            position,
-            with: .character,
-            inDirection: UITextDirection(rawValue: UITextLayoutDirection.right.rawValue)
-        ) else { return }
+        let layoutManager = textView.layoutManager
+        let textContainer = textView.textContainer
 
-        let index = textView.offset(from: textView.beginningOfDocument, to: range.start)
-        
-        guard let attachment = textView.attributedText?.attribute(.attachment, at: index, effectiveRange: nil) as? NSTextAttachment else { return }
-        
-        guard let tappedIndex = attachments.firstIndex(of: attachment) else { return }
+        // タップ位置から文字インデックスを取得
+        let characterIndex = layoutManager.characterIndex(for: loc,
+                                                          in: textContainer,
+                                                          fractionOfDistanceBetweenInsertionPoints: nil)
 
-        let images = attachments.compactMap { $0.image }
-        let zoomVC = ImageGalleryViewController(images: images, initialIndex: tappedIndex)
-        zoomVC.modalPresentationStyle = UIModalPresentationStyle.overFullScreen
-        present(zoomVC, animated: false)
+        // インデックス範囲内で attachment を取得
+        if characterIndex < textView.attributedText.length,
+           let attachment = textView.attributedText.attribute(.attachment,
+                                                              at: characterIndex,
+                                                              effectiveRange: nil) as? NSTextAttachment,
+           let tappedIndex = attachments.firstIndex(of: attachment) {
+
+            let images = attachments.compactMap { $0.image }
+            let zoomVC = ImageGalleryViewController(images: images, initialIndex: tappedIndex)
+            zoomVC.modalPresentationStyle = .overFullScreen // 型を明示
+            present(zoomVC, animated: true)
+        }
     }
 
+}
 
+class ImageGalleryViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+    var images: [UIImage]
+    var initialIndex: Int
+    private var collectionView: UICollectionView!
+
+    init(images: [UIImage], initialIndex: Int) {
+        self.images = images
+        self.initialIndex = initialIndex
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        layout.itemSize = view.bounds.size
+
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        collectionView.backgroundColor = .black
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(ImageZoomCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(collectionView)
+
+        collectionView.layoutIfNeeded()
+        collectionView.scrollToItem(at: IndexPath(item: initialIndex, section: 0),
+                                    at: .centeredHorizontally, animated: false)
+
+        addCloseButton()
+    }
+
+    private func addCloseButton() {
+        let btn = UIButton(type: .system)
+        btn.setTitle("×", for: .normal)
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        btn.tintColor = .white
+        btn.frame = CGRect(x: 20, y: 40, width: 50, height: 40)
+        btn.addTarget(self, action: #selector(close), for: .touchUpInside)
+        view.addSubview(btn)
+    }
+
+    @objc private func close() {
+        dismiss(animated: false)
+    }
+
+    // MARK: - CollectionView
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        images.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ImageZoomCell
+        cell.configure(with: images[indexPath.item])
+        return cell
+    }
+}
+
+class ImageZoomCell: UICollectionViewCell, UIScrollViewDelegate {
+    private let scrollView = UIScrollView()
+    private let imageView = UIImageView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        scrollView.frame = contentView.bounds
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        scrollView.minimumZoomScale = 1
+        scrollView.maximumZoomScale = 3
+        scrollView.delegate = self
+        contentView.addSubview(scrollView)
+
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = scrollView.bounds
+        imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        scrollView.addSubview(imageView)
+    }
+
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+
+    func configure(with image: UIImage) {
+        imageView.image = image
+    }
+
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
 }
 
 //
